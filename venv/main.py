723d145a65026c792232eb6110fc88ce45a27a26
Younes Nikbin <younesnikbin@gmail.com>
2023-02-11 00:15:35 -0800

    Delete venv directory

--- venv/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from Controller.FirstController import FirstController
-
-app = QApplication(sys.argv)
-window = FirstController()
-window.view.show()
-sys.exit(app.exec())
